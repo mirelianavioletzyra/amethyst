24ce093da37bef971925234f2adc2f7ddae960d3
Moriah Gibbs <moriah@protechig.com>
2016-05-23 14:28:10 -0400

    first section of front page and mobile responsive for header and nav on iphone

--- front-page.php
+++ front-page.php
@@ -7,21 +7,22 @@ remove_action('genesis_loop', 'genesis_do_loop');
 add_action('genesis_before_content_sidebar_wrap', 'front_page_content');
 
 function front_page_content() {
-  echo 'helo dicks';
 
   ?>
 
-  <div class="hero">
-    <h1></h1>
-    <span class="tagline-1"></span>
-    <span class="tagline-2"></span>
+  <div class="hero" style="background: url('<?php the_field( 'hero_img' ); ?>')">
+    <div class="inner-wrap">
+      <h1><?php the_field( 'headding' ); ?></h1>
+      <h2 class="tagline-1"><?php the_field( 'tagline_one' ); ?></h2>
+      <h3 class="tagline-2"><?php the_field( 'tagline_two' ); ?></h3>
 
-    <section class="buttons">
-      <a href="#" class="purple-button"></a>
-      <a href="#" class="light-purple-button"></a>
-    </section>
+      <section class="buttons">
+        <a href="<?php the_field( 'button_one_link' ); ?>" class="purple-button"><?php the_field( 'button_one_text' ); ?></a>
+        <a href="<?php the_field( 'button_two_link' ); ?>" class="light-purple-button"><?php the_field( 'button_two_text' ); ?></a>
+      </section>
 
-    <a href="#" class="down-button"><img src="#"></a>
+      <a href="<?php the_field( 'down_arrow_link' ); ?>" class="down-button"><img src="<?php the_field( 'down_arrow' ); ?>"></a>
+    </div>
   </div>
 
   <div class="about">
